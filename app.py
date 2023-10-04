5d1135ef4bdbcff1785a123863c6e73fdc95a187
kcs-imrankazi <imran.kazi@kcsitglobal.com>
2023-10-04 17:32:07 +0530

    updated requirements.txt10

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL ("/")
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Welcome to Render Cloud Platform.."
 
 if __name__ == "__main__":
     ## Run the app when this script is executed
