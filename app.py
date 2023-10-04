7c57b10756a63b8118325a4c0cb8b347f5799331
alokkumardas01 <146924813+alokkumardas01@users.noreply.github.com>
2023-10-04 13:38:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    # Run the app when this script is executed
+    app.run()
